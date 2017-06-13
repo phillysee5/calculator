b74998a149f5957dae018f18fe9a078713144aa5
phillip michael townshend <845862126@ken20-202-18.local>
2017-06-14 10:43:36 +1200

    slight styling changes, beginning to script calculator functions

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 $("button").click(function(){
   var value = $(this).text();
-  console.log(value);
+  var numeral = /([0-9])/;
+  var operator = /([+-x/])/;
+  var equals = /([=])/;
+  var currentvalue = $("#current").text();
+    if(value.match(numeral) ){
+      $("#current").append(value);
+    }
+    else if (value.match(operator) ){
+      localStorage.setItem('firstvalue', currentvalue);
+      $("#current").text(value);
+      console.log(localStorage);
+    }
+  // else if (value.match(equals)) {
+  //
+  // }
+
+
 });
-
-
-"#current"
